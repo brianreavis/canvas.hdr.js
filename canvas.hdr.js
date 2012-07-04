4acb5197404423f9f8d5eba9cf6f613a74e1a345
Brian Reavis <brian@thirdroute.com>
2012-07-04 12:38:43 -0700

    Fixed alpha compositing. Implemented drawImage() (follows the spec). Modularized component blending.

--- canvas.hdr.js
+++ canvas.hdr.js
@@ -44,7 +44,7 @@
 	// globals
 	// --------------------------------------------------------------
 	
-	window.HDR2D_BLEND_NONE     = 0;
+	window.HDR2D_BLEND_NORMAL   = 0;
 	window.HDR2D_BLEND_ADD      = 1;
 	window.HDR2D_BLEND_SUBTRACT = 2;
 	
@@ -88,6 +88,7 @@
 			var context = {
 				canvas: canvas,
 				globalAlpha: 1,
+				globalBlendMode: HDR2D_BLEND_NORMAL,
 				imageData: imageData2DHDR,
 				range: {
 					r: {low: 0, high: 255},
@@ -132,38 +133,55 @@
 				return result;
 			};
 			
+			/**
+			 * Returns a function used for blending individual color components,
+			 * given a particular blend mode.
+			 *
+			 * @param {int}  mode - HDR2D_BLEND_NORMAL, HDR2D_BLEND_ADD, etc.
+			 */
+			var getBlendFunction = (function() {
+				var modes = {};
+				
+				var composite = function(comp_base, comp_new, alpha) {
+					return comp_base + (comp_new - comp_base) * (alpha / 255);
+				};
+				
+				modes[HDR2D_BLEND_NORMAL] = function(comp_base, comp_new, alpha) {
+					return composite(comp_base, comp_new, alpha);
+				};
+				modes[HDR2D_BLEND_ADD] = function(comp_base, comp_new, alpha) {
+					return composite(comp_base, comp_base + comp_new, alpha);
+				};
+				modes[HDR2D_BLEND_SUBTRACT] = function(comp_base, comp_new, alpha) {
+					return composite(comp_base, Math.max(0, comp_base - comp_new), alpha);
+				};
+				
+				return function(mode) {
+					return modes[mode];
+				};
+			})();
+			
 			// public methods
 			// -------------------------------------------------------
 		
 			/**
 			 * Sets the color at the given position.
-			 * @param {int}    x     -  x coordinate.
+			 * @param {int}    x     - x coordinate.
 			 * @param {int}    y     - y coordinate.
 			 * @param {object} color - An object containing r, g, b, a color components.
 			 */
-			context.setPixel = function(x, y, color, mode) {
+			context.setPixel = function(x, y, color) {
 				var i = x + y * CANVAS_WIDTH;
+				var blend = getBlendFunction(context.globalBlendMode);
 				
 				if (typeof color.a === 'undefined') {
 					color.a = 255;
 				}
 				
-				if (typeof mode === 'undefined' || mode === HDR2D_BLEND_NONE) {
-					imageData2DHDR.data[i]   = color.r;
-					imageData2DHDR.data[i+1] = color.g;
-					imageData2DHDR.data[i+2] = color.b;
-					imageData2DHDR.data[i+3] = color.a;
-				} else if (mode === HDR2D_BLEND_ADD) {
-					imageData2DHDR.data[i]   += color.r;
-					imageData2DHDR.data[i+1] += color.g;
-					imageData2DHDR.data[i+2] += color.b;
-					imageData2DHDR.data[i+3] += color.a;
-				} else if (mode === HDR2D_BLEND_SUBTRACT) {
-					imageData2DHDR.data[i]   = Math.max(0, imageData2DHDR.data[i] - color.r);
-					imageData2DHDR.data[i+1] = Math.max(0, imageData2DHDR.data[i+1] - color.g);
-					imageData2DHDR.data[i+2] = Math.max(0, imageData2DHDR.data[i+2] - color.b);
-					imageData2DHDR.data[i+3] = Math.max(0, imageData2DHDR.data[i+3] - color.a);
-				}
+				imageData2DHDR.data[i]   = blend(imageData2DHDR.data[i], color.r, color.a);
+				imageData2DHDR.data[i+1] = blend(imageData2DHDR.data[i+1], color.g, color.a);
+				imageData2DHDR.data[i+2] = blend(imageData2DHDR.data[i+2], color.b, color.a);
+				imageData2DHDR.data[i+3] = Math.min(255, imageData2DHDR.data[i+3] + alpha);
 				
 				// TODO: render this pixel
 			};
@@ -192,7 +210,6 @@
 			 * @param {int} y      - y coordinate (px).
 			 * @param {int} width  - width of the slice (px).
 			 * @param {int} height - height of the slice (px).
-			 * @returns {}
 			 */
 			context.getImageData = function(x, y, width, height) {
 				if (x === 0 && y === 0 && width === canvas_height && height === canvas_height) {
@@ -200,6 +217,72 @@
 				} else {
 					return getImageDataSlice(x, y, width, height);
 				}
+			};
+			
+			/**
+			 * Draws an image to the canvas.
+			 * (for drop-in replacement)
+			 *
+			 * @param {object} image - HTMLImageElement or HTMLCanvasElement or HTMLVideoElement.
+			 * @param {number} dx - Destination x coordinate (px).
+			 * @param {number} dy - Destination x coordinate (px).
+			 * @param {number} dWidth - Destination width (px).
+			 * @param {number} dHeight - Destination height (px).
+			 */
+			context.drawImage = function(image, dx, dy, dWidth, dHeight) {
+				var i, x, y, alpha;
+				var args = arguments;
+				
+				var sx = 0;
+				var sy = 0;
+				var sWidth = image.width;
+				var sHeight = image.height;
+				
+				if (arguments.length > 5) {
+					sx = args[1];
+					sy = args[2];
+					sWidth = args[3];
+					sHeight = args[4];
+					dx = args[5];
+					dy = args[6];
+					dWidth = args[7];
+					dHeight = args[8];
+				}
+				
+				dx = dx || 0;
+				dy = dy || 0;
+				dWidth = dWidth || sWidth;
+				dHeight = dHeight || sHeight;
+				
+				// draw and transform image natively
+				var canvas_tmp = document.createElement('canvas');
+				canvas_tmp.width = CANVAS_WIDTH;
+				canvas_tmp.height = CANVAS_HEIGHT;
+					
+				var context_tmp = canvas.getContext('2d');
+				context_tmp.globalAlpha = 1;
+				context_tmp.drawImage.apply(context_tmp, args);
+				
+				// composite image onto hdr canvas
+				var blend = getBlendFunction(context.globalBlendMode);
+				var data  = context_tmp.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+				var x_min = Math.floor(dx);
+				var y_min = Math.floor(dy);
+				var x_max = Math.ceil(dx, dWidth);
+				var y_max = Math.ceil(dy, dHeight);
+				for (y = y_min; y < y_max; y++) {
+					for (x = x_min; x < x_max; x++) {
+						i = dy * CANVAS_WIDTH + dx;
+						alpha = data[i+3] * context.globalAlpha;
+						imageData2DHDR[i] = blend(imageData2DHDR[i], data[i], alpha);
+						imageData2DHDR[i+1] = blend(imageData2DHDR[i+1], data[i+1], alpha);
+						imageData2DHDR[i+2] = blend(imageData2DHDR[i+2], data[i+2], alpha);
+						imageData2DHDR[i+3] = Math.min(255, imageData2DHDR[i+3] + alpha);
+					}
+				}
+				
+				delete context_tmp;
+				delete canvas_tmp;
 			};
 			
 			/**
